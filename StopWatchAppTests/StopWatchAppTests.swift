1419b33720d159fa886fca7d5b404cdc97492a04
Jobin Joseph <jj@jobins-macbook-air.local>
2019-05-28 11:12:40 -0400

    Release Try

--- StopWatchAppTests/StopWatchAppTests.swift
+++ StopWatchAppTests/StopWatchAppTests.swift
@@ -11,23 +11,41 @@ import XCTest
 
 class StopWatchAppTests: XCTestCase {
     
+    var viewController: ViewController!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
+        viewController = ViewController()
         
+        
+
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        viewController = nil
         super.tearDown()
+
+
     }
     
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-       
+        
+        //Check Timer
+//        let testTimer = 10
+//        XCTAssertEqual(viewController.currentTime, testTimer, "success time is 0")
+        
+
+    }
+    
+    //For Boolean
+    func XCTAssertTrue(_ expression: @autoclosure () throws -> Bool, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line)  {
         
     }
     
